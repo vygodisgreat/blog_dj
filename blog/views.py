fae80532c22ee5c565806848e3d67f65a5f73886
vy <vygodisgreat2003@gmail.com>
2024-01-29 11:31:10 +0530

    added post delete feature

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView # former for database, latter for ?
-from django.views.generic import CreateView, UpdateView
+from django.views.generic import CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
 from .models import Post
+
 # Create your views here.
 
 class BlogListView(ListView):
@@ -21,3 +23,8 @@ class BlogUpdateView(UpdateView):
     model = Post
     template_name = 'post_edit.html'
     fields = ['title', 'body']
+    
+class BlogDeleteView(DeleteView):
+    model = Post
+    template_name = 'post_delete.html'
+    success_url = reverse_lazy('home')
